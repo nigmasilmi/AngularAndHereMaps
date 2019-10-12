fcb2792d48d690a966d93044b54320675f6be282
nigmasilmi <nigmasilmi.web@gmail.com>
2019-10-12 12:13:40 -0300

    app.use added for fixing mixed content error in deploy

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
+// app.use for fixing the mixed content errors in Heroku deploy
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+    }
+  });
+  
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/hereWeGo'));
 
